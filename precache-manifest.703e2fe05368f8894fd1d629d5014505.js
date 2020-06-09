bff42d671b68ee3b5df44704420be21eda476f84
olros <olafrosendahl@gmail.com>
2020-06-09 13:34:29 +0200

    Updates

---
+++ precache-manifest.703e2fe05368f8894fd1d629d5014505.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a820b136c81d3c6d9b7fc3599e5cf01c",
+    "url": "/virtual-stand/index.html"
+  },
+  {
+    "revision": "8bc0bd264f2673383862",
+    "url": "/virtual-stand/static/css/main.a4aef633.chunk.css"
+  },
+  {
+    "revision": "6b8dae6f8e247773f7c6",
+    "url": "/virtual-stand/static/js/2.2f05f73a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/virtual-stand/static/js/2.2f05f73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc0bd264f2673383862",
+    "url": "/virtual-stand/static/js/main.1d13d4e3.chunk.js"
+  },
+  {
+    "revision": "905161cd7cb2471d1c4a",
+    "url": "/virtual-stand/static/js/runtime-main.b7b5a740.js"
+  },
+  {
+    "revision": "90e23d9a38172b193a4f5a7630e6a76c",
+    "url": "/virtual-stand/static/media/NTNUILogo.90e23d9a.svg"
+  },
+  {
+    "revision": "3028a02c0b900addcf7b711d291b73bc",
+    "url": "/virtual-stand/static/media/genfors.3028a02c.jpg"
+  },
+  {
+    "revision": "ca16e155c003b22479555771999f99fa",
+    "url": "/virtual-stand/static/media/innebandy.ca16e155.jpg"
+  },
+  {
+    "revision": "8ea5ee23508196f746997228ba37624e",
+    "url": "/virtual-stand/static/media/rugby.8ea5ee23.jpg"
+  },
+  {
+    "revision": "83af13dfb5d6af61cc901b92393b7958",
+    "url": "/virtual-stand/static/media/studenterhytta.83af13df.jpg"
+  },
+  {
+    "revision": "a6936840631078edf0db5613df52dbe0",
+    "url": "/virtual-stand/static/media/studentlekene.a6936840.jpg"
+  },
+  {
+    "revision": "a74920f56cbe6eab0b653afad5498d8b",
+    "url": "/virtual-stand/static/media/stup.a74920f5.jpg"
+  },
+  {
+    "revision": "b9d477551fa3f2dcb581595dabefdd95",
+    "url": "/virtual-stand/static/media/triatlon.b9d47755.jpg"
+  }
+]);
