4b1bfe9c27a4dfbdb7ca87dcb69b64c5fc2930a1
olros <olafrosendahl@gmail.com>
2020-07-12 23:08:59 +0200

    Add iframe title

--- src/containers/ActiveCampus/index.js
+++ src/containers/ActiveCampus/index.js
@@ -33,6 +33,12 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '1.2rem',
     margin: '5px 0',
   },
+  iframe: {
+    border: '2px solid ' + theme.colors.background.primaryLight,
+    borderRadius: 10,
+    margin: 'auto',
+    background: theme.colors.background.primaryLight,
+  },
 }));
 
 function ActiveCampus(props) {
@@ -52,7 +58,7 @@ function ActiveCampus(props) {
         <p className={classes.text}>Ved NTNU vet vi at for å prestere best akademisk, trenger studentene et godt studiemiljø. aktivCampus er et samarbeid mellom NTNU, NTNUI, Sit og Velferdstinget med mål å senke terskelen for å være sunn og aktiv ved NTNU. Det blir økende grunnlag for å si at jevnlig fysisk aktivitet fører til bedre fysisk og psykisk helse, i tillegg til å bedre akademiske prestasjoner.</p>
         <p className={classes.text}>Hos <a href='https://helsenorge.no/trening-og-fysisk-aktivitet/hva-fysisk-aktivitet-gjor-med-kroppen'>helsenorge.no</a> kan du lese om enda flere grunner til å holde seg aktiv. Disse er fordeler alle studenter burde nyte av, og fysisk aktivitet bør være en naturlig del avhverdagen ved NTNU. Kampanjen skal synliggjøre mulighetene for å være aktiv ved NTNU i dag og gjøre det lettere å spise sunt. Samarbeidspartnerne skal også jobbe sammen for å skape et bedre rammeverk for aktivitet ved universitetet og sikre at fremtidens campus blir en sporty og aktivCampus.</p>
         <p className={classes.text}>Uke 35, altså rett etter fadderukene, er aktivCampus-uken! Denne uken skjer det mye sprekt på de ulike campusene. Lik <a href='https://www.facebook.com/aktivcampus/'>Facebook-siden</a> vår for å bli oppdatert om hva som skjer. Påmelding åpner i august – Følg med!</p>
-        <iframe src='https://ntnui.no/arrangementer/aktivcampus/' width='100%' height='800px' frameBorder='none' />
+        <iframe className={classes.iframe} src='https://ntnui.no/arrangementer/aktivcampus/' width='100%' height='800px' frameBorder='none' title="Aktiviteter under aktivCampus" />
       </div>
     </Navigation>
   );
